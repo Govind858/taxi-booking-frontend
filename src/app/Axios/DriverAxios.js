a7b69036d8c90b09497b4378d79cc564d05864c7
govind858 <govindjayakumar858@gmail.com>
2025-07-15 21:54:55 +0530

    edited the axios

--- src/app/Axios/DriverAxios.js
+++ src/app/Axios/DriverAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 const DriverAxios = axios.create({
-    baseURL:"http://localhost:5000/taxi-driver/",
+    // baseURL:"http://localhost:5000/taxi-driver/",
+    baseURL:"https://taxi-booking-server.onrender.com",
     headers:{"Content-Type":"Application/json"}
 })
 
--- src/app/Axios/UserAxios.js
+++ src/app/Axios/UserAxios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const UserAxios = axios.create({
-  baseURL: "http://localhost:5000/user/",
+  // baseURL: "http://localhost:5000/user/",
+  baseURL:"https://taxi-booking-server.onrender.com",
   headers: { "Content-Type": "Application/json" }
 });
 
